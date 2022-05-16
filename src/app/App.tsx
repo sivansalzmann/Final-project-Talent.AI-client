2b8447e31c12b9ed01ee69b8f1e4e0b788a08cfd
SivanSalzmann <sivansalzmann10@gmail.com>
2022-05-16 21:04:22 +0300

    update

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,33 +7,32 @@ import { useCookies } from "react-cookie";
 import { useNavigate } from "react-router-dom";
 
 const App: FC = () => {
-  // const [cookie, setCookie] = useCookies(["user"]);
-  // const navigate = useNavigate();
+  const [cookie, setCookie] = useCookies(["user"]);
+  const navigate = useNavigate();
 
-  // let user: any = "";
-  // if (Array.isArray(cookie.user)) {
-  //   user = cookie.user[0];
-  // } else {
-  //   user = cookie.user;
-  // }
+  let user: any = "";
+  if (Array.isArray(cookie.user)) {
+    user = cookie.user[0];
+  } else {
+    user = cookie.user;
+  }
 
-  // const logout = () => {
-  //   fetch(`http://52.215.114.42:3000/api/auth/logout`)
-  //     .then((result) => {
-  //       setCookie("user", "");
-  //       navigate("/");
-  //     })
-  //     .catch((err) => console.log(err));
-  // };
+  const logout = () => {
+    fetch(`http://52.215.114.42:3000/api/auth/logout`)
+      .then((result) => {
+        setCookie("user", "");
+        navigate("/");
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <>
-      <h1>hello world</h1>
-      {/* <AppBar user={user} logout={logout} />
+      <AppBar user={user} logout={logout} />
       <HeaderWrapper id="home">
         <HomePage />
       </HeaderWrapper>
-      <Footer /> */}
+      <Footer />
     </>
   );
 };
